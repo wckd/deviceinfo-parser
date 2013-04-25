2d344ad5b51e892803c5fe17c6b27767fae02f16
Alexander Hansen <alexander@geekevents.org>
2013-04-25 11:29:38 +0300

    Easier handling of utf-8

--- parser.py
+++ parser.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python
 import plistlib, sys, os, glob, csv
+import unicodecsv
 
-reload(sys)
-sys.setdefaultencoding( "utf-8" )
 path = ''
-f = open(sys.argv[1], 'wt')
+if len(sys.argv) < 2:
+        f = open('out.csv', 'wt')
+else:
+        f = open(sys.argv[1], 'wt')
 
 try:
-    writer = csv.writer(f)
+    writer = unicodecsv.writer(f, encoding='utf-8')
     writer.writerow( ('Owner', 'Email', 'MAC-adress', 'Serialnumber', 'IMEI', 'Devicename', 'Version', 'Type') )
     for infile in glob.glob( os.path.join(path, '*.deviceinfo') ):
-	device = plistlib.Plist.fromFile(infile)
+        device = plistlib.Plist.fromFile(infile)
 
-	writer.writerow(( 
-		(device.get("ownerName")), 
-		(device.get("ownerEmail")), 
-		(device.get("deviceWiFiMACAddress")), 
-		(device.get("deviceSerialNumber")), 
-		(device.get("deviceIMEI")), 
-		(device.get("deviceName")), 
-		(device.get("deviceProductVersion")), 
-		(device.get("deviceClass")) 
-		))
+        writer.writerow((
+                (device.get("ownerName")),
+                (device.get("ownerEmail")),
+                (device.get("deviceWiFiMACAddress")),
+                (device.get("deviceSerialNumber")),
+                (device.get("deviceIMEI")),
+                (device.get("deviceName")),
+                (device.get("deviceProductVersion")),
+                (device.get("deviceClass"))
+                ))
 finally:
     f.close()
